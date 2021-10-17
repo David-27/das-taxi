b1b65e5be97a8109b16f54d2513c6fb81c778d2c
Artewer <60617675+Artewer@users.noreply.github.com>
2021-10-17 09:36:18 +0200

    Delete dataset.py

--- dataset.py
+++
@@ -1,5 +0,0 @@
-day_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-month_31 = ['January', 'March', 'May', 'July', 'August', 'October', 'December']
-month_29 = ['February']
-month_30 = ['April', 'June', 'September', 'November']
